77b8175f338ce461e0255e2df55e94ac6f8bbd46
Daniel Teixeira <dc.daniiel@gmail.com>
2020-07-25 10:37:25 -0300

    add gitignore

---
+++ binary_search.py
@@ -0,0 +1,21 @@
+def binary_search(arr, item):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = int((low + high) / 2)
+        hit = arr[mid]
+
+        print("HISTORY: ", mid)
+        if hit == item:
+            return mid
+        if hit > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return None
+
+
+
+print(binary_search(range(0, 1000), 347))
